e507a48bac1e3f4359092979ec914a1651fe32ae
Jasmin <code@jasi.dev>
2025-07-14 16:28:34 +0200

    wip

--- home/programs/hyprland/settings.nix
+++ home/programs/hyprland/settings.nix
@@ -2,6 +2,10 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
+      env = [
+        "LIBGL_ALWAYS_SOFTWARE=1"
+        "WLR_RENDERER_ALLOW_SOFTWARE=1"
+      ];
       monitor = [ ",2560x1440@144,auto,1" ];
       "$mod" = "SUPER";
       bind = [
